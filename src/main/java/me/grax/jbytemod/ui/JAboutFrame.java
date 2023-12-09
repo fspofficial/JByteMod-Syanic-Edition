62390000dac69ec189e94f2da831f2fd5727bc13
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2023-12-09 16:19:13 +0100

    recode aboutPanel

--- src/main/java/me/grax/jbytemod/ui/JAboutFrame.java
+++ src/main/java/me/grax/jbytemod/ui/JAboutFrame.java
@@ -12,40 +12,41 @@ import java.awt.event.ActionListener;
 public class JAboutFrame extends JDialog {
 
     public JAboutFrame(JByteMod jbm) {
-        this.setTitle(JByteMod.res.getResource("about") + " " + jbm.getTitle());
-        this.setModal(true);
-        setBounds(100, 100, 400, 300);
-        JPanel cp = new JPanel();
-        cp.setLayout(new BorderLayout());
-        cp.setBorder(new EmptyBorder(10, 10, 10, 10));
+        initializeUI(jbm);
+    }
+
+    private void initializeUI(JByteMod jbm) {
+        setTitle(JByteMod.res.getResource("about") + " " + jbm.getTitle());
+        setModal(true);
+        setBounds(100, 100, 450, 300);
+        JPanel contentPanel = new JPanel(new BorderLayout());
+        contentPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         setResizable(false);
-        JButton close = new JButton(JByteMod.res.getResource("close"));
-        close.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JAboutFrame.this.dispose();
-            }
-        });
-        JPanel jp = new JPanel(new GridLayout(1, 4));
-        for (int i = 0; i < 3; i++)
-            jp.add(new JPanel());
 
-        jp.add(close);
-        cp.add(jp, BorderLayout.PAGE_END);
+        JButton closeButton = new JButton(JByteMod.res.getResource("close"));
+        closeButton.addActionListener(e -> dispose());
 
-        JTextPane title = new JTextPane();
-        title.setContentType("text/html");
-        title.setText(TextUtils.toHtml(jbm.getTitle()
-                + "<br/>Copyright \u00A9 2016-2018 noverify<br/><br/>Copyright \u00A9 2019 Panda<br/><br/>Copyright \u00A9 2020 xBrownieCodez<br/><br/>JByteMod by noverify, Reborn by Panda, Remastered by xBrownieCodez<br/><font color=\"#0000EE\"><u>https://github.com/GraxCode/JByteMod-Beta</u></font><br/>Donate LTC: <font color=\"#333333\">LhwXLVASzb6t4vHSssA9FQwq2X5gAg8EKX</font>"));
-        UIDefaults defaults = new UIDefaults();
-        defaults.put("TextPane[Enabled].backgroundPainter", this.getBackground());
-        title.putClientProperty("Nimbus.Overrides", defaults);
-        title.putClientProperty("Nimbus.Overrides.InheritDefaults", true);
-        title.setBackground(null);
-        title.setEditable(false);
-        title.setBorder(null);
-        cp.add(title, BorderLayout.CENTER);
+        JPanel buttonPanel = new JPanel(new GridLayout(1, 4));
+        for (int i = 0; i < 3; i++) {
+            buttonPanel.add(new JPanel());
+        }
+        buttonPanel.add(closeButton);
 
-        getContentPane().add(cp);
+        JTextPane titlePane = new JTextPane();
+        titlePane.setContentType("text/html");
+        titlePane.setText(TextUtils.toHtml(
+                jbm.getTitle() +
+                        "<br/>Copyright \u00A9 2016-2018 noverify<br/><br/>" +
+                        "Copyright \u00A9 2019 Panda<br/><br/>" +
+                        "Copyright \u00A9 2020-2023 xBrownieCodez<br/><br/>" +
+                        "JByteMod by noverify, Reborn by Panda, Remastered by xBrownieCodez" +
+                        "<br/><font color=\"#0000EE\"><u>https://github.com/xBrownieCodezV2/JByteMod-Remastered</u></font>"
+        ));
+        titlePane.setEditable(false);
+
+        contentPanel.add(buttonPanel, BorderLayout.PAGE_END);
+        contentPanel.add(titlePane, BorderLayout.CENTER);
+
+        getContentPane().add(contentPanel);
     }
 }
