fe592d4016f5646e9d210837538ad8a1928b04aa
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2024-09-18 00:53:09 +0200

    feat 2.8.1: semantic versioning

--- src/main/java/de/xbrowniecodez/jbytemod/utils/update/objects/Version.java
+++ src/main/java/de/xbrowniecodez/jbytemod/utils/update/objects/Version.java
@@ -8,30 +8,45 @@ import lombok.Setter;
 public final class Version {
     private final int majorVersion;
     private final int minorVersion;
+    private final int patchVersion;
 
-    public Version(int majorVersion, int minorVersion) {
+    public Version(int majorVersion, int minorVersion, int patchVersion) {
         this.majorVersion = majorVersion;
         this.minorVersion = minorVersion;
+        this.patchVersion = patchVersion;
     }
 
     public Version(String version) {
         String[] split = version.split("\\.");
-        this.majorVersion = Integer.parseInt(split[0]);
-        this.minorVersion = Integer.parseInt(split[1]);
+        // Ensure there are at least 3 parts; if not, default to 0 for missing parts
+        this.majorVersion = split.length > 0 ? Integer.parseInt(split[0]) : 0;
+        this.minorVersion = split.length > 1 ? Integer.parseInt(split[1]) : 0;
+        this.patchVersion = split.length > 2 ? Integer.parseInt(split[2]) : 0;
     }
-
     /**
      * Compares this version to another version.
      *
      * @param version The version to compare to.
-     * @return true if the version is greater than the other version, false otherwise.
+     * @return true if this version is greater than the other version, false otherwise.
      */
     public boolean isNewer(Version version) {
-        return (majorVersion > version.majorVersion)
-                || (minorVersion > version.minorVersion);
+        if (this.majorVersion > version.majorVersion) {
+            return true;
+        } else if (this.majorVersion < version.majorVersion) {
+            return false;
+        } else { // majorVersion is equal
+            if (this.minorVersion > version.minorVersion) {
+                return true;
+            } else if (this.minorVersion < version.minorVersion) {
+                return false;
+            } else { // minorVersion is equal
+                return this.patchVersion > version.patchVersion;
+            }
+        }
     }
 
+
     public String toString() {
-        return String.format("%d.%d", majorVersion, minorVersion);
+        return String.format("%d.%d.%d", majorVersion, minorVersion, patchVersion);
     }
 }
